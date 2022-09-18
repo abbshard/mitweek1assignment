d079a0a97da044437aa159589403c472909278a5
abiodun sukurat <39300785+abbshard@users.noreply.github.com>
2022-09-18 08:21:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+//program to print hello world using echo
+ echo 'Hello World';
+echo '<br>';
+//program to print hello php using variable
+$greetings = 'Hello PHP';
+echo $greetings;
+echo '<br>';
+//program to print basic data types in variable
+//php string can be single or double quotes.
+$x = "Hello";
+$y = 'Hello PHP';
+echo $x;
+echo '<br>';
+echo $y;
+echo '<br>';
+//php accept integer and is must not have a decimal point
+$b = 123;
+echo $b;
+echo '<br>';
+//php accept float is a number with decimal point
+$c = 10.24;
+echo $c;
+echo '<br>';
+//php accept boolean that is true or false
+$d = true;
+$da = false;
+// echo d printed 1 for true
+echo $d;
+echo '<br>';
+echo $da;
